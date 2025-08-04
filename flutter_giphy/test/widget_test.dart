436bad70593f5a2d4b158d40d7801ffb571746f0
Kristaps-M <kristapsmitins@gmail.com>
2025-08-04 23:34:25 +0300

    add GifDetailView test

--- flutter_giphy/test/widget_test.dart
+++ flutter_giphy/test/widget_test.dart
@@ -8,7 +8,10 @@
 // Visit https://flutter.dev/docs/cookbook/testing/widget/introduction for
 // more information about Widget testing.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_giphy/src/giphy_feature/gif_detail_view.dart';
 import 'package:flutter_giphy/src/giphy_feature/giphy_item_details_view.dart';
 import 'package:flutter_giphy/src/giphy_feature/giphy_item_list_view.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -48,6 +51,34 @@ void main() {
       );
 
       expect(find.text('Flutter Mobile App.'), findsOneWidget);
+    });
+
+    setUpAll(() => HttpOverrides.global = null);
+    testWidgets('Giphy DetailView title and Gif\'s title is visible',
+        (tester) async {
+      const title = 'My Title';
+      const rating = "g";
+      const gifUrl = "https://picsum.photos/200/300";
+      const originalHeight = 225.0;
+      const originalWidth = 400.0;
+
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: GifDetailView(
+            title: title,
+            rating: rating,
+            gifUrl: gifUrl,
+            width: originalWidth,
+            height: originalHeight,
+          ),
+        ),
+      );
+
+      await tester.pump(); // Let the widget settle
+
+      expect(find.text('GIF Detail'), findsOneWidget);
+      expect(find.text('Title: My Title'), findsOneWidget);
+      expect(find.text('Rating: g'), findsOneWidget);
     });
   });
 }
